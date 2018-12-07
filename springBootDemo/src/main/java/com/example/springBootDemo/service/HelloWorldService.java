225a3942a45100b73787fd9c3bfa9f54caf3c8ab
xiyangxishi <masque_v@163.com>
2018-12-07 17:09:25 +0800

    test git account

---
+++ springBootDemo/src/main/java/com/example/springBootDemo/service/HelloWorldService.java
@@ -0,0 +1,13 @@
+package com.example.springBootDemo.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @description 
+ * @author ShuJinJing
+ * @time 2018年12月7日 下午5:08:47 
+ */
+@Service
+public class HelloWorldService {
+
+}
